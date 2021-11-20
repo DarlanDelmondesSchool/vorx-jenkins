d2a08ece4a7eff7584828527dc40d828c6ecc501
Darlan <darlan.delmondes@live.com>
2021-11-20 18:22:31 +0000

    first commit]

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "======================================"
+echo "===== Building Vorx Docker Image ====="
+echo "======================================"
+
+docker-compose build --no-cache
